678d3d8425561e06b937c0371298c8a9da23dc13
Julio Lima <july@Julios-MacBook-Pro.local>
2022-03-21 01:49:42 -0400

    removed some imports

--- src/main/java/com/example/playground/controller/HomeController.java
+++ src/main/java/com/example/playground/controller/HomeController.java
@@ -1,21 +1,15 @@
 package com.example.playground.controller;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class HomeController {
